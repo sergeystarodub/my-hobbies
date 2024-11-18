bd955859afe06a74e0d6f4a1b527bf08c754864c
sergeystarodub <starodub.sergey0785@gmail.com>
2024-11-18 10:55:31 +0500

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Спасибо, что уделили внимание. Я был рад рассказать Вам о своих интересах. 😉';
+})
